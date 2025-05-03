a36b2dfaad2b2c5dfc743d6e93e9938947e6125b
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2025-05-03 10:40:56 -0500

    Add auto-setup of camera and light for random OBJ file uploads.

--- src/components/ObjViewer.tsx
+++ src/components/ObjViewer.tsx
@@ -11,13 +11,15 @@ function ObjViewer({ file }: ObjViewerProps) {
   const sceneRef = useRef<THREE.Scene>(null);
   const cameraRef = useRef<THREE.PerspectiveCamera>(null);
   const rendererRef = useRef<THREE.WebGLRenderer>(null);
+  const lightsRef =
+    useRef<Record<"dirLightXMinus" | "dirLightXPlus", THREE.DirectionalLight>>(null);
 
   const loadObj = useCallback((file: File) => {
     if (!sceneRef.current) return;
 
     const fileReader = new FileReader();
     fileReader.onload = () => {
-      if (!sceneRef.current || !cameraRef.current) return;
+      if (!sceneRef.current || !cameraRef.current || !lightsRef.current) return;
 
       const fileData = fileReader.result;
       if (typeof fileData !== "string") return;
@@ -26,7 +28,33 @@ function ObjViewer({ file }: ObjViewerProps) {
       const objFile = objLoader.parse(fileData);
 
       sceneRef.current.add(objFile);
+
+      const boundingBox = new THREE.Box3().setFromObject(objFile);
+      const boundingSphere = new THREE.Sphere();
+      boundingBox.getBoundingSphere(boundingSphere);
+
+      const radius = boundingSphere.radius;
+
+      const vFov = cameraRef.current.fov * (Math.PI / 180);
+      const hFov = 2 * Math.atan(cameraRef.current.aspect * Math.tan(vFov / 2));
+
+      const vDistance = radius / Math.tan(vFov / 2);
+      const hDistance = radius / Math.tan(hFov / 2);
+      const distance = Math.max(vDistance, hDistance) * 1.5;
+
+      cameraRef.current.position.copy(boundingSphere.center).add(new THREE.Vector3(0, 0, distance));
+      cameraRef.current.lookAt(boundingSphere.center);
+
+      lightsRef.current.dirLightXPlus.position
+        .copy(boundingSphere.center)
+        .add(new THREE.Vector3(radius, 0, 0));
+      lightsRef.current.dirLightXPlus.target.position.copy(boundingSphere.center);
+      lightsRef.current.dirLightXMinus.position
+        .copy(boundingSphere.center)
+        .add(new THREE.Vector3(-radius, 0, 0));
+      lightsRef.current.dirLightXMinus.target.position.copy(boundingSphere.center);
     };
+
     fileReader.readAsText(file);
   }, []);
 
@@ -38,23 +66,24 @@ function ObjViewer({ file }: ObjViewerProps) {
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 1000);
-    camera.position.set(-0.1, 0.5, 1);
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize(width, height);
     objViewerWrapperRef.current.appendChild(renderer.domElement);
 
-    const dirLight1 = new THREE.DirectionalLight(0xffffff, 1);
-    const dirLight2 = new THREE.DirectionalLight(0xffffff, 1);
-    dirLight1.position.set(-20, 70, 20);
-    dirLight2.position.set(20, 70, 20);
+    const dirLightXPlus = new THREE.DirectionalLight(0xffffff, 1);
+    const dirLightXMinus = new THREE.DirectionalLight(0xffffff, 1);
 
-    scene.add(dirLight1);
-    scene.add(dirLight2);
+    scene.add(dirLightXPlus);
+    scene.add(dirLightXMinus);
 
     sceneRef.current = scene;
     cameraRef.current = camera;
     rendererRef.current = renderer;
+    lightsRef.current = {
+      dirLightXPlus,
+      dirLightXMinus,
+    };
 
     const animate = () => {
       requestAnimationFrame(animate);
